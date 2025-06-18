a01829ad8c25983f96f8aad47e1ef155bab487ae
Pule Molefe <pmolefe25@gmail.com>
2025-06-18 06:23:33 -0700

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(document).ready(() => {
+	$('body').append('<p>ALX Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<p>Copyright - ALX</p>');
+});
