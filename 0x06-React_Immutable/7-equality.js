22f1ed53b4b200c917f943785fbc8a82d3b3892d
Pule Molefe <pmolefe25@gmail.com>
2025-06-18 04:01:41 -0700

    Equality

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,5 +1,13 @@
-import { is } from 'immutable';
+import { is, Map } from 'immutable';
 
+/**
+ * Compares two Immutable.js Maps for deep equality.
+ *
+ * @param {Map} map1 - The first Immutable Map
+ * @param {Map} map2 - The second Immutable Map
+ * @returns {boolean} - True if both Maps are equal
+ */
+\
 export default function areMapsEqual(Map1, Map2) {
 	return is(map1, map2);
 }
