f76e64af5d1d1d863d4cdfcb107f7a12bb2c4274
Pule Molefe <pmolefe25@gmail.com>
2025-06-24 09:22:54 -0700

    Write mapStateToProps

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.js
@@ -1,3 +1,18 @@
 // App.js
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+
+function App({ isLoggedIn }) {
+	return (
+		<div>
+			<h1>Dashboard</h1>
+			{isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
+		</div>
+	);
+}
+
+const mapStateToProps = (state) => ({
+	isLoggedIn: state.ui.isLoggedIn,
+});
+
+export default connect(mapStateToProps)(App);
