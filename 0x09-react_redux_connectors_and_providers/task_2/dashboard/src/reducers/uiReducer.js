2247d83c6807e6a6ed870e211464cc9f5eb67c2d
Pule Molefe <pmolefe25@gmail.com>
2025-06-24 22:32:07 -0700

    Modify the uiReducer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,14 +1,23 @@
 const initialState = {
 	isLoggedIn: false,
 	isNotificationDrawerVisible: false,
+	user: null,
 };
 
 export default function uiReducer(state = initialState, action) {
 	switch (action.type) {
 		case 'LOGIN' :
-			return { ...state, isLoggedIn: true };
+			return {
+				...state,
+				isLoggedIn: true,
+				user: action.user,
+			};
 		case 'LOGOUT':
-			return { ...state, isLoggedIn: false };
+			return {
+				...state,
+				isLoggedIn: false,
+				user: null,
+			};
 		case 'SHOW_NOTIFICATION_DRAWER':
 			return { ...state, isNotificationDrawerVisible: true };
 		case 'HIDE_NOTIFICATION_DRAWER':
