98d870efdfa425eaddbb65da6d42fae19e7000cf
Pule Molefe <pmolefe25@gmail.com>
2025-06-24 20:49:09 -0700

    Implement Redux notification drawer toggle with tests

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
@@ -2,13 +2,15 @@ import { fromJS } from 'immutable';
 import { mapStateToProps } from './App';
 
 describe('mapStateToProps', () => {
-	it('should return the correct isLoggedIn prop from state', () => {
+	it('should return the correct props from state', () => {
 		const state = fromJS({
-			isUserLoggedIn: true
+			isUserLoggedIn: true,
+			isNotificationDrawerVisible: true,
 		});
 
 		const expectedProps = {
-			isLoggedIn: true
+			isLoggedIn: true,
+			displayDrawer: true,
 		};
 
 		expect(mapStateToProps(state)).toEqual(expectedProps);
