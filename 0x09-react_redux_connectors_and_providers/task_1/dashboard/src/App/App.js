eaf416804c3ba9aa8018a0bdf3224d26dc100dee
Pule Molefe <pmolefe25@gmail.com>
2025-06-24 20:31:23 -0700

    Connect your actions creators

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
@@ -1,16 +1,21 @@
 // App.js
 import React from 'react';
 import { connect } from 'react-redux';
+import { displayNotificationDrawer, hideNotificationDrawer } from '../actions/uiActionCreators';
 
-export function App({ isLoggedIn, displayDrawer }) {
+export function App({ isLoggedIn, displayDrawer, displayNotificationDrawer, hideNotificationDrawer }) {
 	return (
 		<div>
 			<h1>Dashboard</h1>
 			{isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
+
 			{displayDrawer ? (
-				<div className="drawer">🔔 Notifications Drawer</div>
+				<div className="drawer">
+				🔔 Notifications Drawer
+				<button onClick={hideNotificationDrawer}>Hide Drawer</button>
+				</div>
 			) : (
-				<p>No notifications</p>
+				<button onClick={displayNotificationDrawer}>Show Notifications</button>
 			)}
 		</div>
 	);
@@ -20,5 +25,11 @@ export const mapStateToProps = (state) => ({
 	isLoggedIn: state.get('isUserLoggedIn'),
 	displayDrawer: state.get('isNotificationDrawerVisible'),
 });
+
+// Using object shorthand syntax
+export const mapDispatchToProps = {
+	displayNotificationDrawer,
+	hideNotificationDrawer,
+};
 
 export default connect(mapStateToProps)(App);
